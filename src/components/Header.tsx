2bc34bd3dec1f8b6eb35edd75c07bc65c46726b0
Omar <omarguaba54@gmail.com>
2025-05-14 12:27:51 -0400

    a value fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import { useSearchParams, useRouter } from 'next/navigation';
 export default function Header() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const a = 0;
+
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
