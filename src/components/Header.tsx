f608a60c3c5fbf10efd44af303f64a44ffeaf942
Omar <omarguaba54@gmail.com>
2025-05-14 12:38:23 -0400

    header updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,30 +4,15 @@ import { FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
-import { useEffect, useState } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+
 export default function Header() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(searchParams);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if (searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl);
-    }
-  }, [searchParams]);
+ 
+  
 
   
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams(searchParams);
-    urlParams.set('searchTerm', searchTerm);
-    const searchQuery = urlParams.toString();
-    router.push(`/search?${searchQuery}`);
-  };
+
+  
+  
   
   return (
     <header className='bg-slate-200 shadow-md'>
@@ -40,14 +25,17 @@ export default function Header() {
         </Link>
         <form
           className='bg-slate-100 p-3 rounded-lg flex items-center'
-          onSubmit={handleSubmit}
+      
+          
         >
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+           
+            
+          
+            
           />
           <button>
             <FaSearch className='text-slate-600' />
